ab361071246fb155e80656753abdeb90b16c8c45
Ravikant108 <ravikantkrsingh108@gmail.com>
2026-07-07 06:20:31 +0530

    Update mainUrl for NetflixMirrorProvider

--- ReflexMirror/src/main/kotlin/com/reflex1337/reflexmirror/NetflixMirrorProvider.kt
+++ ReflexMirror/src/main/kotlin/com/reflex1337/reflexmirror/NetflixMirrorProvider.kt
@@ -27,7 +27,7 @@ class NetflixMirrorProvider : MainAPI() {
     )
     override var lang = "en"
 
-    override var mainUrl = "https://net52.cc"
+    override var mainUrl = "https://net11.cc"
     override var name = "Netflix"
 
     override val hasMainPage = true
