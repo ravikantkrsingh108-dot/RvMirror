36579558db1170de0c7749b9e6a3d799b8d47105
Ravikant108 <ravikantkrsingh108@gmail.com>
2026-07-17 22:35:36 +0530

    Update version and add namespace in build.gradle.kts
    Updated version number and added namespace for Android.

--- ReflexMirror/build.gradle.kts
+++ ReflexMirror/build.gradle.kts
@@ -1,7 +1,8 @@
 // use an integer for version numbers
-version = 2
+version = 3
 
 android {
+    namespace = "com.reflex1337.reflexmirror" // <--- YOU MUST ADD THIS LINE
     buildFeatures {
         buildConfig = true
     }
@@ -15,25 +16,13 @@ dependencies {
 
 cloudstream {
     language = "en"
-    // All of these properties are optional, you can safely remove them
-
     description = "Netflix, PrimeVideo, Disney+ Hotstar Contents in Multiple Languages"
     authors = listOf("Reflex1337, NivinCNC")
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     * */
-    status = 1 // will be 3 if unspecified
+    status = 1
     tvTypes = listOf(
         "Movie",
         "TvSeries"
     )
-
     requiresResources = false
-
     iconUrl = "https://raw.githubusercontent.com/ravikantkrsingh108-dot/RvMirror/refs/heads/main/Logos/ReflexMirror-icon.png"
 }
