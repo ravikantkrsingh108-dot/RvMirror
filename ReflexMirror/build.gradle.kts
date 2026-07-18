2d8c45e4518e88c85e82015a3122c24012529a25
Ravikant108 <ravikantkrsingh108@gmail.com>
2026-07-18 07:23:08 +0530

    Update build.gradle.kts

--- ReflexMirror/build.gradle.kts
+++ ReflexMirror/build.gradle.kts
@@ -1,18 +1,14 @@
 // use an integer for version numbers
-version = 3
+version = 4 // Bumped to 4 to force CloudStream to recognize the update
 
 android {
-    namespace = "com.reflex1337.reflexmirror" // <--- YOU MUST ADD THIS LINE
+    namespace = "com.reflex1337.reflexmirror"
     buildFeatures {
         buildConfig = true
     }
 }
 
-dependencies {
-    implementation("androidx.core:core:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-}
+// Do NOT add any implementation dependencies here! It breaks the .cs3 file.
 
 cloudstream {
     language = "en"
