3c60b1e11da9ec4e1e0fed4df1ffd2a783235b24
Ravikant108 <ravikantkrsingh108@gmail.com>
2026-07-19 07:22:13 +0530

    Update version and dependencies in build.gradle.kts
    Updated version number and added implementation dependencies for Android libraries. Modified cloudstream properties including description and status.

--- ReflexMirror/build.gradle.kts
+++ ReflexMirror/build.gradle.kts
@@ -1,24 +1,39 @@
 // use an integer for version numbers
-version = 4
+version = 6
 
 android {
-    namespace = "com.reflex1337.reflexmirror"
     buildFeatures {
         buildConfig = true
     }
 }
 
-// Do NOT add any implementation dependencies here! It breaks the .cs3 file.
+dependencies {
+    implementation("androidx.core:core:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+}
 
 cloudstream {
     language = "en"
+    // All of these properties are optional, you can safely remove them
+
     description = "Netflix, PrimeVideo, Disney+ Hotstar Contents in Multiple Languages"
     authors = listOf("Reflex1337, NivinCNC")
-    status = 1
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
     tvTypes = listOf(
         "Movie",
         "TvSeries"
     )
+
     requiresResources = false
+
     iconUrl = "https://raw.githubusercontent.com/ravikantkrsingh108-dot/RvMirror/refs/heads/main/Logos/ReflexMirror-icon.png"
 }
