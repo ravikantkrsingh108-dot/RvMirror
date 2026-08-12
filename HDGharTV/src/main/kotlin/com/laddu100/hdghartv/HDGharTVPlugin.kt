06855349e149cccbc5a5ef0f70248690c1d699e0
Ravikant108 <ravikantkrsingh108@gmail.com>
2026-08-13 00:05:57 +0530

    Modify load method to include Context and init storage
    Updated the load method to accept a Context parameter and initialized local storage. Also added a comment for the new smart catalog registration.

--- HDGharTV/src/main/kotlin/com/laddu100/hdghartv/HDGharTVPlugin.kt
+++ HDGharTV/src/main/kotlin/com/laddu100/hdghartv/HDGharTVPlugin.kt
@@ -1,12 +1,14 @@
 package com.laddu100.hdghartv
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class HDGharTVPlugin : Plugin() {
-    override fun load() {
+    override fun load(context: Context) {
+        HDGharTVStorage.init(context.applicationContext) // Initialize local storage
         registerMainAPI(HDGharTVProvider())
-        registerMainAPI(HDGharTVSmartProvider())
+        registerMainAPI(HDGharTVSmartProvider()) // Register the new smart catalog
     }
 }
