6679c4cf68969be1a8c6b765404ffb6bb07a85de
Ravikant108 <ravikantkrsingh108@gmail.com>
2026-09-03 13:59:11 +0530

    Add new streaming categories to RogmoviesProvider

--- VegaMovies/src/main/kotlin/com/megix/RogmoviesProvider.kt
+++ VegaMovies/src/main/kotlin/com/megix/RogmoviesProvider.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.utils.*
 import org.json.JSONObject
 import kotlinx.coroutines.runBlocking
 
-class RogmoviesProvider : VegaMoviesProvider() { // all providers must be an instance of MainAPI
+class RogmoviesProvider : VegaMoviesProvider() {
     override var mainUrl = "https://rogmovies.vip"
     override var name = "Rogmovies"
     override val hasMainPage = true
@@ -42,8 +42,18 @@ class RogmoviesProvider : VegaMoviesProvider() { // all providers must be an ins
     override val mainPage = mainPageOf(
         "$mainUrl/page/%d/" to "Home",
         "$mainUrl/category/web-series/netflix/page/%d/" to "Netflix",
-        "$mainUrl/category/web-series/disney-plus-hotstar/page/%d/" to "Disney Plus Hotstar",
         "$mainUrl/category/web-series/amazon-prime-video/page/%d/" to "Amazon Prime",
+        "$mainUrl/category/web-series/disney-plus-hotstar/page/%d/" to "Disney+ Hotstar",
+        "$mainUrl/category/web-series/zee5/page/%d/" to "ZEE5",
+        "$mainUrl/category/web-series/sonyliv/page/%d/" to "SonyLIV",
+        "$mainUrl/category/web-series/jio-cinema/page/%d/" to "JioCinema",
         "$mainUrl/category/web-series/mx-original/page/%d/" to "MX Original",
+        "$mainUrl/category/anime-series/page/%d/" to "Anime Series",
+        "$mainUrl/category/korean-series/page/%d/" to "Korean Series",
+        "$mainUrl/category/hindi-movies/page/%d/" to "Hindi Movies",
+        "$mainUrl/category/south-hindi-dubbed/page/%d/" to "South Hindi Dubbed",
+        "$mainUrl/category/dual-audio/page/%d/" to "Dual Audio",
+        RANDOM_MOVIES to "🔀 Movies Shuffle",
+        RANDOM_SERIES to "🔀 Series Shuffle"
     )
 }
